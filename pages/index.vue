b983f2ca7ce3181966674c39a8e1d8f445773e71
syunsuke okamura <syunrobin@gmail.com>
2022-06-15 22:40:52 +0900

    ✒ index.vue : calender cells

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,11 @@
 		<div class="calender">
 			<table>
 				<tr class="Firrow"> <th class="Sun Fircol">Sunday</th> <th class="Mon Seccol">Monday</th> <th class="Tue Thicol">Tuesday</th> <th class="Wed Foucol">Wednesday</th> <th class="Thu Fifcol">Thursday</th> <th class="Fri sixcol">Friday</th> <th class="Sat sevcol">Saturday</th> </tr>
+				<tr class="Secrow"> <th class="_1 Fircol"></th> <th class="_2 Seccol"></th> <th class="_3 Thicol"></th> <th class="_4 Foucol"></th> <th class="_5 Fifcol"></th> <th class="_6 sixcol"></th> <th class="_7 sevcol"></th> </tr>
+				<tr class="Thirow"> <th class="_8 Fircol"></th> <th class="_9 Seccol"></th> <th class="_10 Thicol"></th> <th class="_11 Foucol"></th> <th class="_12 Fifcol"></th> <th class="_13 sixcol"></th> <th class="_14 sevcol"></th> </tr>
+				<tr class="Fourow"> <th class="_15 Fircol"></th> <th class="_16 Seccol"></th> <th class="_17 Thicol"></th> <th class="_18 Foucol"></th> <th class="_19 Fifcol"></th> <th class="_20 sixcol"></th> <th class="_21 sevcol"></th> </tr>
+				<tr class="Fifrow"> <th class="_22 Fircol"></th> <th class="_23 Seccol"></th> <th class="_24 Thicol"></th> <th class="_25 Foucol"></th> <th class="_26 Fifcol"></th> <th class="_27 sixcol"></th> <th class="_28 sevcol"></th> </tr>
+				<tr class="Sixrow"> <th class="_29 Fircol"></th> <th class="_30 Seccol"></th> <th class="_31 Thicol"></th> <th class="_32 Foucol"></th> <th class="_33 Fifcol"></th> <th class="_34 sixcol"></th> <th class="_35 sevcol"></th> </tr>
 			</table>
 		</div>
 		<div class="tasklist">
@@ -14,3 +19,7 @@
 export default {
 }
 </script>
+
+<style scoped>
+
+</style>

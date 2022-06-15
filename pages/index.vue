e221ec6b7d89a0bfc713a19121e8924733627cc0
syunsuke okamura <syunrobin@gmail.com>
2022-06-15 22:24:29 +0900

    ✒ index.vue : first low of calender

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="content">
 		<div class="calender">
+			<table>
+				<tr class="Firrow"> <th class="Sun Fircol">Sunday</th> <th class="Mon Seccol">Monday</th> <th class="Tue Thicol">Tuesday</th> <th class="Wed Foucol">Wednesday</th> <th class="Thu Fifcol">Thursday</th> <th class="Fri sixcol">Friday</th> <th class="Sat sevcol">Saturday</th> </tr>
+			</table>
 		</div>
 		<div class="tasklist">
 		</div>
@@ -9,10 +12,5 @@
 
 <script>
 export default {
-
 }
 </script>
-
-<style>
-
-</style>
